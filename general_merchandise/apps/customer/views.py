99168d9281d0070c7041decba3af4db3cf4045e9
Jan Dale Amores <amoresjan14@gmail.com>
2020-09-11 04:46:29 +0800

    added inheritance

--- general_merchandise/apps/customer/views.py
+++ general_merchandise/apps/customer/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def customerDashboardViewSet(request):
-    return render(request, 'customer/dashboard.html')
+    customers = Person.objects.all()
+    return render(request, 'customer/dashboard.html', {'customers': customers})
 
 def customerRegistrationViewSet(request):
     return render(request, 'customer/registration_customer.html')
