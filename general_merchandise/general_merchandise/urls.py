b237dd92a14dc58bc34af94645c8feb2904022c4
davegamboa99 <53077307+davegamboa99@users.noreply.github.com>
2020-09-10 14:32:33 +0800

    new branch(2)

--- general_merchandise/general_merchandise/urls.py
+++ general_merchandise/general_merchandise/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
+    path('', include('apps.landing_page.urls')),
     path('customer/', include('apps.customer.urls')),
     path('product/', include('apps.product.urls')),
     path('admin/', admin.site.urls),
