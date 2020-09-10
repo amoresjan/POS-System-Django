5af6d0be6d89e6b8741208e991e621f41e010b01
Jan Dale Amores <amoresjan14@gmail.com>
2020-09-11 00:02:38 +0800

    added datepicker

--- general_merchandise/static/apps/customer/js/dashboard-table.js
+++ general_merchandise/static/apps/customer/js/dashboard-table.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".data-table").DataTable({
+  var oTable = $(".data-table").DataTable({
     dom: "Bfrtip",
     buttons: [
       {
@@ -10,5 +10,58 @@ $(document).ready(function () {
       },
     ],
   });
+
+  $("#datepicker_from")
+    .datepicker({
+      showOn: "button",
+      buttonImage: "images/calendar.gif",
+      buttonImageOnly: false,
+      onSelect: function (date) {
+        minDateFilter = new Date(date).getTime();
+        oTable.draw();
+      },
+    })
+    .keyup(function () {
+      minDateFilter = new Date(this.value).getTime();
+      oTable.draw();
+    });
+
+  $("#datepicker_to")
+    .datepicker({
+      showOn: "button",
+      buttonImage: "images/calendar.gif",
+      buttonImageOnly: false,
+      onSelect: function (date) {
+        maxDateFilter = new Date(date).getTime();
+        oTable.draw();
+      },
+    })
+    .keyup(function () {
+      maxDateFilter = new Date(this.value).getTime();
+      oTable.draw();
+    });
 });
-``;
+
+// Date range filter
+minDateFilter = "";
+maxDateFilter = "";
+
+$.fn.dataTableExt.afnFiltering.push(function (oSettings, aData, iDataIndex) {
+  if (typeof aData._date == "undefined") {
+    aData._date = new Date(aData[3]).getTime();
+  }
+
+  if (minDateFilter && !isNaN(minDateFilter)) {
+    if (aData._date < minDateFilter) {
+      return false;
+    }
+  }
+
+  if (maxDateFilter && !isNaN(maxDateFilter)) {
+    if (aData._date > maxDateFilter) {
+      return false;
+    }
+  }
+
+  return true;
+});
