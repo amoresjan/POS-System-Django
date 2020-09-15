5e36172adf221844b277719c0e42b15dec2c8231
Jan Dale Amores <amoresjan14@gmail.com>
2020-09-15 11:57:20 +0800

    altered model Person

---
+++ apps/customer/choices.py
@@ -0,0 +1,18 @@
+MALE = 'M'
+FEMALE = 'F'
+Sex = (
+    (MALE, 'Male'),
+    (FEMALE, 'Female')
+)
+
+SINGLE = 'S'
+MARRIED = 'M'
+WIDOWED = 'W'
+DIVORCED = 'D'
+Status = (
+    (SINGLE, 'Single'),
+    (MARRIED, 'Married'),
+    (WIDOWED, 'Widowed'),
+    (DIVORCED, 'Divorced')
+)
+
